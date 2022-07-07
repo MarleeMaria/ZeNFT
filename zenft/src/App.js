f71c8f0655d0448a4d9d17c82fc1a31e7297b917
Marlee Roth <marlee.roth@gmail.com>
2022-07-07 12:27:19 -0400

    add react app

--- zenft/src/App.js
+++ zenft/src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useEffect, useState } from "react"
+import { useCookies } from "react-cookie"
 function App() {
+
+
+  
+const [nft, setNft]=useState()
+const [image, setImage]=useState()
+
+const [cookies, setCookie ] = useCookies(['click']);
+const [disable, setDisable ] = useState();
+
+
+console.log(cookies.click)
+
+
+// useEffect(() => {
+
+// if (cookies.click){
+//   setDisable("disabled-link")
+// }  });
+
+
+function roll(){
+  const number = Math.floor(Math.random() * 416)
+  setNft(number)
+
+    import(`./output/${number}.png`).then((module) => {
+    setImage(module.default);
+  });
+
+}
+
+function claim(){
+
+   setCookie('click', true, "/")
+
+setDisable("disabled-link")
+
+}
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>
+          Welcome to ZeNFT
+        </h1>
+       <div><h2>Click Below to roll for you ZeNFT</h2></div>
+        <br></br>
+      <img src={image} ></img>
+        <br></br>
+
+     <button type="button" onClick={roll}>ROLL!</button>
+<button>
+     <a type="button" 
+     href={image}
+     download
+     onClick={claim}
+class={disable}
+       >Claim (this can only be done once)</a>
+    </button>
+
     </div>
   );
 }
